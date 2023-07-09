31a7b6204db679bd81dbd649763956561a0ddce1
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-09 15:40:52 +0100

    added files

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Yahoo Clone</h1>
+    <header className="header">
+      <div className="logo">
+        <img src="https://s.yimg.com/rz/p/yahoo_frontpage_en-US_s_f_p_bestfit_frontpage_2x.png" alt="Yahoo Logo" />
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search Yahoo" />
+        <button type="button" className="search-button">
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
     </header>
   );
 }
 
 export default Header;
+
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,28 +1,32 @@
 import React from 'react';
 
 const NewsList = () => {
-  // Assuming you have an array of news articles
   const newsArticles = [
     {
       id: 1,
       title: 'Breaking News',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      image: 'https://via.placeholder.com/150',
     },
     {
       id: 2,
       title: 'Latest Updates',
       description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      image: 'https://via.placeholder.com/150',
     },
     // Add more news articles as needed
   ];
 
   return (
-    <div>
-      <h2>News List</h2>
+    <div className="news-list">
+      <h2>News</h2>
       {newsArticles.map((article) => (
-        <div key={article.id}>
-          <h3>{article.title}</h3>
-          <p>{article.description}</p>
+        <div key={article.id} className="news-card">
+          <img src={article.image} alt={article.title} />
+          <div className="news-content">
+            <h3>{article.title}</h3>
+            <p>{article.description}</p>
+          </div>
         </div>
       ))}
     </div>
